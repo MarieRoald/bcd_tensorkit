ff41d4a99006c0c6b21b1c98e0216bcdc59ab8b0
Marie Roald <roald.marie@gmail.com>
2020-08-30 08:22:30 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,7 @@
+=================
+Block tenkit ADMM
+=================
+
+
+Old code for block-coordinate descent parafac2
+
---
+++ src/block_tenkit_admm/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+__author__ = "Marie Roald"
+__email__ = "mariero@simula.no"
+__version__ = "0.0.1"
+
+
+from .block_parafac2 import *
